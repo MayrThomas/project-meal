acdab0fa05d3ad18cdbf8c766b07289fce8dbca9
Thomas Mayr <tmayr1099@gmail.com>
2023-07-19 18:54:14 +0200

    add placeholder image for grid item

--- projectMeal/app/src/main/java/at/thomas/mayr/projectMeal/view/RecipeGridItem.kt
+++ projectMeal/app/src/main/java/at/thomas/mayr/projectMeal/view/RecipeGridItem.kt
@@ -8,37 +8,46 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import at.thomas.mayr.projectMeal.R
 import at.thomas.mayr.projectMeal.room.entities.RecipeWithIngredient
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun RecipeGridItem(recipeWithIngredients: RecipeWithIngredient) {
+fun RecipeGridItem(recipeWithIngredients: RecipeWithIngredient, onClick: () -> Unit) {
     Card(
         modifier = Modifier
             .background(MaterialTheme.colorScheme.background)
             .padding(8.dp)
             .border(width = 2.dp, color = MaterialTheme.colorScheme.secondary),
-        shape = RoundedCornerShape(4.dp)
+        shape = RoundedCornerShape(4.dp),
+        onClick = onClick
     ) {
         Column(
-            modifier = Modifier.fillMaxSize().padding(8.dp),
-            verticalArrangement = Arrangement.spacedBy(4.dp)
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
         ) {
-            Image(imageVector = Icons.Filled.Menu,
-                contentDescription = "image of the food from the recipe"
+            Image(
+                ImageBitmap.imageResource(id = R.drawable.placeholder_food),
+                contentDescription = "image of the food from the recipe",
             )
             Text(text = recipeWithIngredients.recipe.name,
                 fontWeight = FontWeight.Bold,
-                overflow = TextOverflow.Visible
+                overflow = TextOverflow.Visible,
+                textAlign = TextAlign.Center
             )
         }
     }
