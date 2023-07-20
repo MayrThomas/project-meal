e2acd723ad33855f1c3083de3aaf8a1bc7469e12
Thomas Mayr <tmayr1099@gmail.com>
2023-07-20 10:51:54 +0200

    add text to item if no recipes are available

--- projectMeal/app/src/main/java/at/thomas/mayr/projectMeal/view/EmptyRecipeGridItem.kt
+++ projectMeal/app/src/main/java/at/thomas/mayr/projectMeal/view/EmptyRecipeGridItem.kt
@@ -1,13 +1,19 @@
 package at.thomas.mayr.projectMeal.view
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import at.thomas.mayr.projectMeal.R
 
 @Composable
@@ -15,10 +21,19 @@ fun EmptyRecipeGridItem() {
     Box(
         Modifier.fillMaxSize()
     ) {
-        Image(
-            bitmap = ImageBitmap.imageResource(id = R.drawable.chef_hat),
-            contentDescription = "Empty recipe list icon",
-            Modifier.align(Alignment.Center)
-        )
+        Column(
+            Modifier.align(Alignment.Center),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            Image(
+                bitmap = ImageBitmap.imageResource(id = R.drawable.chef_hat),
+                contentDescription = "Empty recipe list icon",
+            )
+            Text(
+                text = "No recipes available!!",
+                textAlign = TextAlign.Center,
+                modifier = Modifier.padding(16.dp)
+            )
+        }
     }
 }
